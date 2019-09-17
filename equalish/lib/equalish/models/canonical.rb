f96ce73d67951824153a69a9e90bbcd10bf96deb
Charles Harris <charris000@gmail.com>
2019-09-17 06:16:54 -0500

    Add is_canonical_for? method

--- equalish/lib/equalish/models/canonical.rb
+++ equalish/lib/equalish/models/canonical.rb
@@ -16,6 +16,18 @@ module Equalish
           equivalent_data = yield || []
           @canonicalized[canonical_field.to_sym] = equivalent_data.append(canonical_data)
         end
+
+        def field_eq(equivocal, canonical_field, canonical_data)
+          return equivocal.respond_to?(canonical_field) \
+            && canonical_data.include?(equivocal.send(canonical_field))
+        end
+
+        def is_canonical_for?(equivocal)
+          @canonicalized.any? do |canonical_field, canonical_data|
+            return false unless field_eq(equivocal, canonical_field.to_sym, canonical_data)
+          end
+          true
+        end
       end
 
       def self.included(klass)
